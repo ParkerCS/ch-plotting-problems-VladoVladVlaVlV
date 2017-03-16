b60cd5dfecdee40073eae2e539e8ed6b6cf11d2b
VladoVladVlaVlV <vvojdanovski@fwparker.org>
2017-03-16 11:04:54 -0500

    commit2

--- chi_life_problem.py
+++ chi_life_problem.py
@@ -1,42 +1,42 @@
-#1 Import csv, numpy, and matplotlib.plot
-#2 Open the chi_life_expectancy.txt file
-#3 Use csv.reader(file, delimeter='\t') to read in the file to a list.  Make appropriate lists for plotting. Community name will be the x and 2010 life expectancy on the y.
-#4 Plot the life_expectancy_2010_list vs a numpy arange() as a bar graph
-#5 Use ax = plt.gca() to grab the axes object as ax. Use ax.set_xticklabels(community_list) to place the labels on the x axis, use the kwarg rotation=60 to tilt the lettering since there are a lot of communities
-#6  Set an appropriate plt.ylim([min,max])
-#7  Label your axes
-#8  Add a title
-#9  Add text to indicate the minimum and maximum values
-#10 Customize your graph in at least two other ways using documentation from matplotlib.org
-#11  Comment your code as always.
+# 1 Import csv, numpy, and matplotlib.plot
+# 2 Open the chi_life_expectancy.txt file
+# 3 Use csv.reader(file, delimeter='\t') to read in the file to a list.  Make appropriate lists for plotting. Community name will be the x and 2010 life expectancy on the y.
+# 4 Plot the life_expectancy_2010_list vs a numpy arange() as a bar graph
+# 5 Use ax = plt.gca() to grab the axes object as ax. Use ax.set_xticklabels(community_list) to place the labels on the x axis, use the kwarg rotation=60 to tilt the lettering since there are a lot of communities
+# 6  Set an appropriate plt.ylim([min,max])
+# 7  Label your axes
+# 8  Add a title
+# 9  Add text to indicate the minimum and maximum values
+# 10 Customize your graph in at least two other ways using documentation from matplotlib.org
+# 11  Comment your code as always.
 
 # Note:  If you would like to present something different than the above for your graph using this dataset, just let me know your intentions before you start and I will do my best to support you.
 import csv
 import numpy as np
-import matplotlib.pyplot as plt # we import libraries
-file=open("chi_life_expectancy.txt","r")  # we open the file
-life_list=[]    #make an empty list
+import matplotlib.pyplot as plt  # we import libraries
 
-reader= csv.reader(file, delimiter="\t")    #give instructions for splitting the text
+file = open("chi_life_expectancy.txt", "r")  # we open the file
+life_list = []  # make an empty list
 
-for row in reader:      #split the text up
+reader = csv.reader(file, delimiter="\t")  # give instructions for splitting the text
+
+for row in reader:  # split the text up
     life_list.append(row)
 print(life_list)
 
+community_area_number_list = []
+community_area_list = []
+life_expectancy_90 = []
+lower_ci_90 = []
+upper_ci_90 = []
+life_expectancy_00 = []
+lower_ci_00 = []
+upper_ci_00 = []
+life_expectancy_10 = []
+lower_ci_10 = []
+upper_ci_10 = []  # make empty list for all the information we have
 
-community_area_number_list=[]
-community_area_list=[]
-life_expectancy_90=[]
-lower_ci_90=[]
-upper_ci_90=[]
-life_expectancy_00=[]
-lower_ci_00=[]
-upper_ci_00=[]
-life_expectancy_10=[]
-lower_ci_10=[]
-upper_ci_10=[]     # make empty list for all the information we have
-
-for i in range (1,len(life_list)):
+for i in range(1, len(life_list)):
     community_area_number_list.append(life_list[i][0])
     community_area_list.append(life_list[i][1])
     life_expectancy_90.append(float(life_list[i][2]))
@@ -47,27 +47,24 @@ for i in range (1,len(life_list)):
     upper_ci_00.append(life_list[i][7])
     life_expectancy_10.append(life_list[i][8])
     lower_ci_10.append(life_list[i][9])
-    upper_ci_10.append(life_list[i][10])      #fill up the lists
+    upper_ci_10.append(life_list[i][10])  # fill up the lists
 
-print(community_area_list)     #check if we have the correct information filled
+print(community_area_list)  # check if we have the correct information filled
 
-plt.figure(figsize=[15,7], tight_layout=True)         #adjust width, height and text
+plt.figure(figsize=[15, 7], tight_layout=True)  # adjust width, height and text
 
-plt.bar(np.arange(len(life_expectancy_90)),life_expectancy_90,color="blue",alpha=0.3)  #format the life expectancy list
+plt.bar(np.arange(len(life_expectancy_90)), life_expectancy_90, color="blue",
+        alpha=0.3)  # format the life expectancy list
 
-plt.xticks(np.arange(len(life_expectancy_90)),life_expectancy_10)
-plt.ylim(55,80)
-plt.text(35,57,"minimum=57",color="red",fontsize=15)
-plt.arrow(17,16.4,19.7,40,head_width=0.5,color="red")
-plt.text(70,79,"maximum=79",color="red",fontsize=15)
+plt.xticks(np.arange(len(life_expectancy_90)), life_expectancy_10)
+plt.ylim(55, 80)
+plt.text(35, 57, "minimum=57", color="red", fontsize=15)
+plt.arrow(17, 16.4, 19.7, 40, head_width=0.5, color="red")
+plt.text(70, 79, "maximum=79", color="red", fontsize=15)
 plt.xlabel("Neighborhood")
 plt.ylabel("Life Expectancy")
-plt.title("Life Expectancy in Chicago",fontsize=25)
-ax=plt.gca()
-ax.set_xticklabels(community_area_list,rotation=90)     #add and rotate the text of the community area list
-
-
-
+plt.title("Life Expectancy in Chicago", fontsize=25)
+ax = plt.gca()
+ax.set_xticklabels(community_area_list, rotation=90)  # add and rotate the text of the community area list
 
 plt.show()
-
--- chi_pub_lib_problem.py
+++ chi_pub_lib_problem.py
@@ -1,4 +1,3 @@
-
 # MATPLOTLIB PROBLEM # 1
 # Chicago Public Library Visitors by Month (25pts)
 # open and read in the "chilib_visitors_2016" file into a list
@@ -10,21 +9,23 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-file2=open("chilib_visitors_2016","r")
+import matplotlib.patches as mpatches
+
+file2 = open("chilib_visitors_2016", "r")
 
 ex_list = []
-jan=0
-feb=0
-mar=0
-apr=0
-may=0
-june=0
-july=0
-aug=0
-sept=0
-oct=0
-nov=0
-dec=0
+jan = 0
+feb = 0
+mar = 0
+apr = 0
+may = 0
+june = 0
+july = 0
+aug = 0
+sept = 0
+oct = 0
+nov = 0
+dec = 0
 
 reader = csv.reader(file2, delimiter="\t")
 
@@ -32,40 +33,43 @@ for row in reader:
     ex_list.append(row)
 print(ex_list)
 
-for i in range(1,len(ex_list)):
-    jan+=int(ex_list[i][1])
-    feb+=int(ex_list[i][2])
-    mar+=int(ex_list[i][3])
-    apr+=int(ex_list[i][4])
-    may+=int(ex_list[i][5])
-    june+=int(ex_list[i][6])
-    july+=int(ex_list[i][7])
-    aug+=int(ex_list[i][8])
-    sept+=int(ex_list[i][9])
-    oct+=int(ex_list[i][10])
-    nov+=int(ex_list[i][11])
-    dec+=int(ex_list[i][12])
+for i in range(1, len(ex_list)):
+    jan += int(ex_list[i][1])
+    feb += int(ex_list[i][2])
+    mar += int(ex_list[i][3])
+    apr += int(ex_list[i][4])
+    may += int(ex_list[i][5])
+    june += int(ex_list[i][6])
+    july += int(ex_list[i][7])
+    aug += int(ex_list[i][8])
+    sept += int(ex_list[i][9])
+    oct += int(ex_list[i][10])
+    nov += int(ex_list[i][11])
+    dec += int(ex_list[i][12])
 
-month_list = [jan,feb,mar,apr,may,june,july,aug,sept,oct,nov,dec]
+month_list = [jan, feb, mar, apr, may, june, july, aug, sept, oct, nov, dec]
 
-month_text=["January","February","March","April","May","June","July","August","September","October","November","December"]
+month_text = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+              "November", "December"]
 
-most_suc=[]
-numbers_list=[]
-extra=[]
-for i in range(1,len(ex_list)):
-    x=0
+most_suc = []
+numbers_list = []
+extra = []
+for i in range(1, len(ex_list)):
     most_suc.append(ex_list[i][0])
-    for y in range(1,len(ex_list[i])):
-        x+=int(ex_list[i][y])
-    numbers_list.append(x)
+    for y in range(1, len(ex_list[i])):
+        x = int(ex_list[i][y])
+        numbers_list.append(x)
 
 print(numbers_list)
+numbers_list = [numbers_list[i:i + 13] for i in range(0, len(numbers_list), 13)]
+print(numbers_list)
 
-full_list=[val for pair in zip(most_suc, numbers_list) for val in pair]
+full_list = [val for ten in zip(most_suc, numbers_list) for val in ten]
 print(full_list)
 
-full_list=[full_list[i:i + 2] for i in range(0, len(full_list), 2)]
+full_list = [full_list[i:i + 2] for i in range(0, len(full_list), 2)]
+
 
 def sorting(my_list):
     for pos in range(len(my_list)):
@@ -79,17 +83,31 @@ def sorting(my_list):
 
     print(my_list)
 
+
 sorting(full_list)
 
-plt.figure(figsize=[15,7], tight_layout=True)
+plt.figure(figsize=[15, 7], tight_layout=True)
 
-plt.plot(np.arange(len(month_list)),month_list,color="red")
-plt.plot(np.arange(len(numbers_list),full_list[0][1]),color="blue")
+plt.plot(np.arange(len(month_list)), month_list, color="red")
+plt.plot(np.arange(len(full_list[0][1][:-1])), full_list[0][1][:-1], color="blue")
+plt.plot(np.arange(len(full_list[1][1][:-1])), full_list[1][1][:-1], color="green")
+plt.plot(np.arange(len(full_list[2][1][:-1])), full_list[2][1][:-1], color="brown")
 
-plt.xticks(np.arange(len(month_list)),month_text)
-plt.xticks(np.arange(len(numbers_list)),full_list[0][0])
+plt.xticks(np.arange(len(month_list)), month_text)
+plt.xticks(np.arange(len(full_list[0][1][:-1])), month_text)
+plt.xticks(np.arange(len(full_list[1][1][:-1])), month_text)
+plt.xticks(np.arange(len(full_list[2][1][:-1])), month_text)
 
+total_patch = mpatches.Patch(color="red", label="Total visitors")
+first_patch = mpatches.Patch(color="blue", label=full_list[0][0] + " visitors")
+second_patch = mpatches.Patch(color="green", label=full_list[1][0] + " visitors")
+third_patch = mpatches.Patch(color="brown", label=full_list[2][0] + " visitors")
 
+legend = plt.legend(handles=[total_patch, first_patch, second_patch, third_patch], shadow=False, loc=6)
+legend.set_alpha(0.01)
 
+plt.xlabel("Months", fontsize=15)
+plt.ylabel("Libraries", fontsize=15)
+plt.title("Number of people visiting Chicago Public Libraries", fontsize=20)
 
 plt.show()
